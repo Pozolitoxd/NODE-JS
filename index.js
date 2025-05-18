c39d05cf0c39883d13084769561253d010dae552
pozolitoxd <estebantoro.p.7@gmail.com>
2025-05-18 10:50:18 -0600

    nueva actualizacion

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var operaciones = require('./opreacionesEnteros/enteros.js')
 const exp = require('express')
+var invetario = require('./operacionesObjetos/inventario.js')
 
 const app = exp();
 app.get('/quierosumar',(req,res )=>{
     res.end('la suma de 5 y 6 es: '+operaciones.suma(5,6));
+})
+app.get('/consulta',(req,res)=>{
+    res.end('mirar consola'+invetario.cargarDatos())
 })
 app.listen(8888,()=>{
         console.log('online')
